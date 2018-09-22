e0ea88031b2eb66c7172e1b562c2a478f5982b68
Chris Q <chrisq.@Chriss-MacBook-Air.local>
2018-09-22 16:51:44 -0700

    finished required stories

---
+++ flixster/flixster/Views/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  flixster
+//
+//  Created by Chris Q. on 9/22/18.
+//  Copyright © 2018 Chris Q. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
